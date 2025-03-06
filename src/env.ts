5088df733501141b19dcfba657294d6362a30c29
Siebe Baree <siebe.baree@outlook.com>
2025-03-06 11:05:07 +0100

    Added AI SDK

--- src/env.ts
+++ src/env.ts
@@ -5,11 +5,13 @@ export const env = createEnv({
     server: {
         DATABASE_URL: z.string().url(),
         NODE_ENV: z.enum(["development", "test", "production"]).default("development"),
+        OPENAI_API_KEY: z.string(),
     },
     client: {},
     runtimeEnv: {
         DATABASE_URL: process.env.DATABASE_URL,
         NODE_ENV: process.env.NODE_ENV,
+        OPENAI_API_KEY: process.env.OPENAI_API_KEY,
     },
     skipValidation: !!process.env.SKIP_ENV_VALIDATION,
     emptyStringAsUndefined: true,
