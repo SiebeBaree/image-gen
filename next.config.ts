d1a0829115b6a9f4a5f601da798976080e61f251
Siebe Baree <siebe.baree@outlook.com>
2025-03-06 09:39:55 +0100

    Added security headers and simplified build step

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,58 @@
-import "@/env";
+import { env } from "@/env";
 import type { NextConfig } from "next";
 
-const config = {} as NextConfig;
+const securityHeaders = [
+    {
+        key: "X-DNS-Prefetch-Control",
+        value: "on",
+    },
+    {
+        key: "Strict-Transport-Security",
+        value: "max-age=63072000; includeSubDomains; preload",
+    },
+    {
+        key: "X-Frame-Options",
+        value: "DENY",
+    },
+    {
+        key: "X-Content-Type-Options",
+        value: "nosniff",
+    },
+    {
+        key: "X-XSS-Protection",
+        value: "1; mode=block",
+    },
+    {
+        key: "Referrer-Policy",
+        value: "strict-origin-when-cross-origin",
+    },
+    {
+        key: "Permissions-Policy",
+        value: "camera=(), microphone=(), geolocation=()",
+    },
+    {
+        key: "Content-Security-Policy",
+        value: `default-src 'self'; script-src 'self' 'unsafe-inline'${env.NODE_ENV !== "production" ? " 'unsafe-eval'" : ""}; style-src 'self' 'unsafe-inline'; img-src 'self' blob: data:; font-src 'self'; connect-src 'self'; frame-src; frame-ancestors 'none'; form-action 'self'; upgrade-insecure-requests; base-uri 'self';`,
+    },
+];
+
+const config = {
+    reactStrictMode: true,
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    poweredByHeader: false,
+    async headers() {
+        return [
+            {
+                source: "/:path*",
+                headers: securityHeaders,
+            },
+        ];
+    },
+} as NextConfig;
 
 export default config;
